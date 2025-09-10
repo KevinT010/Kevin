e4d64dbc994cc8dcd61379afe098f2c4200c65ea
KEVN <troostkevin8@gmail.com>
2025-09-10 15:58:04 +0200

    about me

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
+
+
+  getAge(): number {
+    const birthDate = new Date(2002, 4, 2); 
+    const today = new Date();
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  }
+
 }
